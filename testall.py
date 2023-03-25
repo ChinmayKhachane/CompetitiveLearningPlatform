3f50b5f802473fbfd78c6c207397f50f54689f73
ChinmayKhachane <34871969+ChinmayKhachane@users.noreply.github.com>
2023-03-24 19:02:39 -0700

    Update testall.py
    Changed to compare all possibilities

--- testall.py
+++ testall.py
@@ -3,15 +3,18 @@ import os
 
 def check_plagiarism(files, ressy):
     the_f = []
+    fil = files[0]
     for i in range(len(files)):
         x = open(files[i], 'r').read()
         the_f.append(x)
-    result = open(ressy, 'w')
+    result = open(ressy, 'a')
     sum = pycode_similar.detect(the_f, diff_method=pycode_similar.UnifiedDiff, keep_prints=False, module_level=False)
     d = dict((x,y) for x,y in sum)
+    result.write("ref : " + fil + '\n')
     for i in d.values():
         nus = str((i[0]))
         result.write(nus + '\n')   
+    result.write('\n')
     result.close()
 
 def get_Files():
@@ -27,10 +30,7 @@ filestoput.pop(0)
 
 resi = 'temp.txt'
 
-
-check_plagiarism(filestoput, resi)
-
-
-
-
-
+for j in range(len(filestoput)):
+    check_plagiarism(filestoput, resi)
+    back = filestoput.pop(0)
+    filestoput.append(back)
